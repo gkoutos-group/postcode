67e24a3d90bdec596a01314f3951ace11117265e
Victor Roth Cardoso <vxr610@bham.ac.uk>
2020-02-04 11:49:30 +0000

    collector improvement message

--- integrator/collector.py
+++ integrator/collector.py
@@ -287,7 +287,7 @@ class DataCollector:
                 try:
                     chunk = chunk.merge(ndf, on=d.reference, how='left', copy=False, validate='many_to_one') #merge the data using the reference variables
                 except pd.errors.MergeError as e:
-                    raise ObtainDataError("Data extractor '{}' failed. There are duplicate elements. Please disable it. Duplicated elements are '{}'.".format(d, "', '".join([str(i) for i in ndf[d.reference][ndf[d.reference].duplicated(keep=False)].unique()]))) from pd.errors.MergeError()
+                    raise ObtainDataError("Data extractor '{}' failed. There are duplicate elements. Please disable it. Duplicated elements are '{}'.".format(d, "', '".join([str(i) for i in ndf[d.reference][ndf[d.reference].duplicated(keep=False)].unique()]))) from pd.errors.MergeError() # TODO: this will fail if we have multiple columns
                 if self.verbose:
                     print("|- Source '{}' took {:.2f}s (internal processing {:.2f}s)".format(d, time.time() - start_time, time.time() - internal_time))
             if self.verbose:
